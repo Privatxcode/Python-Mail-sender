b53929d1a4e3c685e993d00dce0608ec0252b757
Privatxcode <140891984+Privatxcode@users.noreply.github.com>
2024-07-03 08:08:09 +0000

    Add files via upload

---
+++ Mailing/send_email.py
@@ -0,0 +1,39 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+# Fonction pour lire le contenu HTML du fichier
+def read_html_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+# Informations de l'email
+from_email = "privt9235@gmail.com"
+to_emails = "privat.daagbo@gozem.co"
+subject = "Promotion de Glace d'été"
+
+# Lire le contenu HTML du fichier
+html_content = read_html_file("index.html")
+
+# Configuration du message
+message = MIMEMultipart("alternative")
+message["Subject"] = subject
+message["From"] = from_email
+message["To"] = ", ".join(to_emails)  # Joindre les adresses email avec une virgule
+
+# Ajouter le contenu HTML
+html_part = MIMEText(html_content, "html")
+message.attach(html_part)
+
+# Mot de passe (utilisez un mot de passe d'application si 2FA est activé)
+password = "zmtl ywkv mykm tnfx"
+
+# Envoyer l'email
+try:
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()  # Démarrer la connexion TLS
+        server.login(from_email, password)
+        server.sendmail(from_email, to_emails, message.as_string())
+        print("Email envoyé avec succès à tous les destinataires")
+except Exception as e:
+    print(f"Erreur lors de l'envoi de l'email: {e}")
